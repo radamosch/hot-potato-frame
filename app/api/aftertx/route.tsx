0d06078b0fc05c0a62d6ad8b1066ce4e9516da99
Rada <radamosch@codestation.local>
2024-03-09 21:50:05 +0100

    button clicker

--- app/api/aftertx/route.tsx
+++ app/api/aftertx/route.tsx
@@ -25,7 +25,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         },
       ],
       image: {
-        src: `${NEXT_PUBLIC_URL}/hot-potato-after.png`,
+        src: `${NEXT_PUBLIC_URL}/hot-potato-2.png`,
         aspectRatio: '1:1',
       },
       input: {
