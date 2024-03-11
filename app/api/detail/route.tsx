220055b78150fe428e23ad7ea2dc42c3552184f0
Rada <radamosch@codestation.local>
2024-03-12 00:44:04 +0100

    contract live

--- app/api/detail/route.tsx
+++ app/api/detail/route.tsx
@@ -18,7 +18,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   const displayCurrentPrice = parseFloat(formatEther(currentPrice as bigint)).toFixed(3);
   //console.log(currentPrice)
   const nextPrice= await contract.read.nextPrice();
-  const displayNextPrice = parseFloat(formatEther(nextPrice as bigint)).toFixed(3);
+  const displayNextPrice = parseFloat(formatEther(nextPrice as bigint)).toFixed(4);
 
   const flipCount= await contract.read.FLIP_COUNT();
 
