7804b8d0f666920f9a4bb2517165e686a5f72bcd
maker <d@videriksson.com>
2024-03-12 09:51:02 +0100

    back to the roots with meta tags

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,12 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
-import { getContract, formatEther , } from 'viem'
-import  client  from './client';
+import { getContract, formatEther, } from 'viem'
+import client from './client';
 
 import type { Metadata } from 'next';
 import { HOT_POTATO_ADDR, NEXT_PUBLIC_URL } from './config';
 
 import abi from './_contracts/HotPotatoAbi';
 
-const  doAsyncWork = async ()=>{
-
-  const contract = getContract({
-    address: HOT_POTATO_ADDR,
-    abi: abi,
-    client,
-  });
-
-  const currentPrice= await contract.read.CURRENT_PRICE();
-  //console.log(currentPrice)
-  const nextPrice= await contract.read.nextPrice();
-  const flipCount= await contract.read.FLIP_COUNT();
-  const blockNumber = await client.getBlockNumber();
-  
-
-
-}
-
-const getButtonPrice = async ()=>{ 
-  
-  // how to get the current price here async?
- 
-  return "ddd";
-}
-
-const frameMetadata = getFrameMetadata(
-{
-  buttons: [
-    {
-      action: 'post',
-      label: `🔥 Tell me! 🔥`,
-      target: `${NEXT_PUBLIC_URL}/api/detail`,
-    }
-  ],
-  image: {
-    src: `${NEXT_PUBLIC_URL}/potato.webp`,
-    aspectRatio: '1:1',
-  },
-  /*
-  input: {
-    text: "Don't click the button!",
-  },*/
-  //postUrl: `${NEXT_PUBLIC_URL}/api/aftertx`,
-});
-
-export const metadata: Metadata = {
-  title: 'Hot Potato!',
-  description: "Hot Potato!",
-  openGraph: {
-    title: 'Hot Potato!',
-    description: "Hot Potato!",
-    images: [`${NEXT_PUBLIC_URL}/potato.webp`],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
 
 export default async function Page() {
 
@@ -73,28 +16,29 @@ export default async function Page() {
     client,
   });
 
-  const currentPrice= await contract.read.CURRENT_PRICE();
+  const currentPrice = await contract.read.CURRENT_PRICE();
   //console.log(currentPrice)
-  const nextPrice= await contract.read.nextPrice();
-  const flipCount= await contract.read.FLIP_COUNT();
+  const nextPrice = await contract.read.nextPrice();
+  const flipCount = await contract.read.FLIP_COUNT();
 
   const blockNumber = await client.getBlockNumber();
 
   return (
     <>
-      <h1>Let's make it hot!</h1>
-      <p>This hot potato is a unique NFT.</p>
-      <p>Every time it flips, it's price increases by 10%</p>
-
-      <p>Current price: {formatEther(currentPrice as bigint)} ETH</p>
-      <p>Current price wei: {(currentPrice as bigint).toString()}</p>
-
-      <p>Next price: {formatEther(nextPrice as bigint)} ETH</p>
-
-      <p>Flip count: {(flipCount as bigint).toString()}</p>
-
-
-      <img src={NEXT_PUBLIC_URL+"/potato.webp"}/>
+      <meta name="og:image" content={`${NEXT_PUBLIC_URL}/potato.webp`} />
+      <meta name="fc:frame" content="vNext" />
+      <meta name="fc:frame:image" content={`${NEXT_PUBLIC_URL}/potato.webp`} />
+      <meta name="fc:frame:post_url" content={`${NEXT_PUBLIC_URL}/api/aftertx`} />
+      <meta name="fc:frame:image:aspect_ratio" content="1:1" />
+      <meta name="fc:frame:button:1" content={`Price: ${parseFloat(formatEther(currentPrice as bigint)).toFixed(3)} E`} />
+      <meta name="fc:frame:button:1:action" content="post" />
+      <meta name="fc:frame:button:1:target" content="#" />
+      <meta name="fc:frame:button:2" content={`Next: ${parseFloat(formatEther(nextPrice as bigint)).toFixed(4)} E`} />
+      <meta name="fc:frame:button:2:action" content="post" />
+      <meta name="fc:frame:button:2:target" content="#" />
+      <meta name="fc:frame:button:3" content="BUY NOW!" />
+      <meta name="fc:frame:button:3:action" content="tx" />
+      <meta name="fc:frame:button:3:target" content={`${NEXT_PUBLIC_URL}/api/potato`} />
     </>
   );
 }
