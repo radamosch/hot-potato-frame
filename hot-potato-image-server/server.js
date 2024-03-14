80a5ff91cf3474d64c9bf7c4af0c9b85aa6ebf0d
maker <d@videriksson.com>
2024-03-14 22:41:34 +0100

    changed the images

--- hot-potato-image-server/server.js
+++ hot-potato-image-server/server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const Jimp = require('jimp');
 const abi = require('./contractABI.json');
 const path = require('path');
-const cors = require('cors') 
+const cors = require('cors')
 
 const { createPublicClient, http, getContract, formatEther } = require('viem')
 const { base } = require('viem/chains')
@@ -23,7 +23,7 @@ const HOT_POTATO_ADDR = "0xb21FC6838708CF3CD539fE17012f9A8aF1C509dC"
 
 
 const app = express();
-app.use(cors()); 
+app.use(cors());
 const port = 3300;
 
 const contract = getContract({
@@ -33,13 +33,13 @@ const contract = getContract({
 });
 
 app.get('/', (req, res) => {
-    req.url='/get-potato'
+    req.url = '/get-potato'
     req.app.handle(req, res);
 })
 
 app.get('/health', (req, res) => {
     res.send("ok");
-  })
+})
 
 app.get('/get-potato', async (req, res) => {
 
@@ -53,34 +53,35 @@ app.get('/get-potato', async (req, res) => {
             await contract.read.FLIP_COUNT()
         ]);
 
-        var imageIndex = parseInt(parseInt(flipCount)/5+""); // step every 5
-        if (imageIndex>9)imageIndex=9; // 9 is the last image
-        const image = await Jimp.read(`./potato-${imageIndex}.png`);
+        var imageIndex = parseInt(parseInt(flipCount) / 5 + ""); // step every 5
+        if (imageIndex > 9) imageIndex = 9; // 9 is the last image
+        const image = await Jimp.read(`./${imageIndex}.jpg`);
 
-        const font = await Jimp.loadFont(Jimp.FONT_SANS_14_BLACK);
+        const ibmFont = await Jimp.loadFont("./fonts/ibm.fnt");
+        const chakraFont = await Jimp.loadFont("./fonts/chakra.fnt");
 
-        image.print(font, 63, 160, "There is only 1!")
-        image.print(font, 63, 184, "EACH TIME IT FLIPS, THE PRICE RISES BY 25%!");
-        image.print(font, 63, 208, "Buy & Share!");
-        image.print(font, 63, 232, "Play at your own risk.");
+        // image.print(ibmFont, 63, 160, "There is only 1!")
+        // image.print(ibmFont, 63, 184, "EACH TIME IT FLIPS, THE PRICE RISES BY 25%!");
+        // image.print(ibmFont, 63, 208, "Buy & Share!");
+        // image.print(ibmFont, 63, 232, "Play at your own risk.");
 
         const currentPriceText = parseFloat(formatEther(currentPrice)).toFixed(3) + " ETH"
         const nextPriceText = parseFloat(formatEther(nextPrice)).toFixed(4) + " ETH"
         const flipCountText = flipCount
 
-        image.print(font, 63, 306, currentPriceText);
-        image.print(font, 63, 406, nextPriceText);
-        image.print(font, 63, 506, flipCountText);
+        image.print(chakraFont, 48, 372, currentPriceText);
+        image.print(chakraFont, 48, 488, nextPriceText);
+        image.print(chakraFont, 48, 602, flipCountText);
 
         const timestamp = Math.floor(Date.now() / 1000);
-        image.print(font, 63, 556, timestamp);
+        //image.print(font, 63, 556, timestamp);
 
-        const outputPath = './potato.png';
+        const outputPath = './potato.jpg';
         await image.writeAsync(outputPath);
-        
+
         res.set('Cache-Control', 'max-age=5');
 
-        res.sendFile(path.join(__dirname, '/potato.png'));
+        res.sendFile(path.join(__dirname, '/potato.jpg'));
 
 
     } catch (error) {
